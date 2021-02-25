e0330adc9f684c917030e8f8ab80ae657bc16d2b
razvan neda <neda_razvan@yahoo.com>
2021-02-25 21:28:25 +0000

    added validations

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,3 +1,10 @@
 class Plant < ApplicationRecord
   has_many  :user_plants 
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :space, presence: true, inclusion: { in: %w(Indoors Outdoors) }
+  validates :light, presence: true
+  validates :type, presence: true, inclusion: { in: %w(flowering fern vegetable fruit succulent)}
+  validates :watering, presence: true
 end
