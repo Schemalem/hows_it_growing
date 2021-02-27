05eb33e723232a2e36bb386a7060cfececb6ea56
razvan neda <neda_razvan@yahoo.com>
2021-02-27 11:10:03 +0000

    added plants controller &views

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,17 @@
+class PlantsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+    @plants = Plant.all
+  end
+  
+  def show
+    @plant = Plant.find(params[:id])
+  end
+  
+  private
+  
+  def plant_params
+    params.require(:plant).permit(:name, :description, :space, :light, :type, :watering, :pet_friendly)
+  end
+end
