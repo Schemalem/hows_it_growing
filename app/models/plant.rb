4609bd90e820b23362a8f0b10e604e2b420a1429
Mags Maras <mmaraska@gmail.com>
2021-03-13 12:11:51 +0000

    changes to the plant light method

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -11,10 +11,10 @@ class Plant < ApplicationRecord
 
   include PgSearch::Model
   pg_search_scope :search_form,
-    against: [ :name, :type, :description, :light, :space, :plant_type, :watering],
+    against: [:name, :type, :description, :light, :space, :plant_type, :watering],
     using: {
       tsearch: { prefix: true }
-    }
+  }
 
   def plant_watering_message
     if watering.include? "1-2 weeks"
@@ -35,17 +35,13 @@ class Plant < ApplicationRecord
       "Medium indirect light to dappled sun"
     elsif light.include? "Low-light to dappled sun"
       "Low light to dappled sun"
-    elsif light.include? "Bright, indirect light to dappled sun"
+    elsif light.include?("Bright, indirect light to dappled sun" || "Bright indirect light to dappled sun")
       "Bright, indirect light to dappled sun"
-    elsif light.include? "Bright indirect light to dappled sun"
-      "Bright indirect light to dappled sun"
     elsif light.include? "Indirect light to dappled sun"
       "Indirect light to dappled sun"
     elsif light.include? "Bright, indirect light. Keep out of direct sunlight"
       "Bright, indirect light. Keep out of direct sunlight."
-    elsif light.include? "Bright, indirect light"
-      "Bright, indirect light"
-    elsif light.include? "Bright, Indirect light"
+    elsif light.include?("Bright, indirect light" || "Bright, Indirect light")
       "Bright, indirect light"
     else "Bright, indirect light"
     end
